1500725419888ac127fed6a2a9d58fd03592e7fa
Elijah <ioluwaseun40@gmail.com>
2025-05-10 18:07:59 +0100

    Update weapp.js to improve functionality

--- weapp.js
+++ weapp.js
@@ -21,7 +21,7 @@ function getWeather() {
             .catch(error => {
             console.error('Error fetching current weather data:', error);
             alert('error fetching current weather data. Please try again.');
-            })
+            });
 }
 function getWeather() {
     fetch(forecastUrl)
@@ -32,5 +32,30 @@ function getWeather() {
             .catch(error => {
             console.error('Error fetching forecast data:', error);
             alert('error fetching forecast data. Please try again.');
-            })
+            });
+}
+
+function displayWeather(data) {
+
+    const tempDivInfo = document.getElementById('temp-div');
+    const weatherInfoDiv = document.getElementById('weather-info');
+    const weatherIcon = document.getElementById('weather-icon');
+    const hourlyForecastDiv = document.getElementById('hourly-forecast');
+
+    // Clear previous content
+    weatherInfoDiv.innerHTML = '';
+    hourlyForecastDiv.innerHTML = '';
+    tempDivInfo.innerHTML = '';
+}
+function displayWeather(data) {
+    if (data.cod === '404') {
+        weatherInfoDiv.innerHTML = '<p>${data.message}</p>';
+    } else {
+
+        const cityName = data.name;
+        const temperature = Math.round(data.main.temp - 273.15);
+        const description = data.weather[0].description;
+        const iconCode = data.weather[0].icon;
+        const iconUrl = 'https://openweathermap.org/img/wn/${iconCode}@4x.png';
+    }
 }
